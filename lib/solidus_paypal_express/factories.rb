c59617a427031daa4fa334939c956af69fff3da0
Jonathan Tapia <jonathan.tapia@magmalabs.io>
2018-06-22 23:54:57 -0500

    Remove unnecessary code

--- lib/solidus_paypal_express/factories.rb
+++ lib/solidus_paypal_express/factories.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :paypal_express_gateway, class: 'Spree::Gateway::PayPalExpress' do
-    # type 'Spree::Gateway::PayPalExpress'
     name 'Paypal Express'
     active true
   end
